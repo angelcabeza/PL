c854d3bd338a07397e760955e65e6ee89a93afeb
angelcabeza <angelcabezamartin@gmail.com>
2021-12-06 19:05:45 +0100

    empiezo semantico

---
+++ estructura_datos.h
@@ -0,0 +1,34 @@
+#ifndef ESTRUCTURAS_DATOS
+#define ESTRUCTURAS_DATOS
+#include <string>
+
+using namespace std;
+
+
+typedef enum {
+    marca,
+    funcion,
+    variable,
+    parametro_formal,
+    fin_bloque
+} tipoEntrada;
+
+typedef enum {
+	entero,
+	real,
+	caracter,
+	booleano,
+	vacio,
+    desconocido,
+    lista
+} dtipo;
+
+typedef struct {
+    tipoEntrada entrada;
+    string nombre = "";
+    dtipo tipoDato = desconocido;
+    int parametros = 0;
+
+} entradaTS;
+
+#endif
